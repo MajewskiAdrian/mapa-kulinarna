e99d7bcb24270507d8daf0f348ee158fea19a83d
SzymonGP <grothaszymon@gmail.com>
2024-11-13 16:32:16 +0100

    Wyświetlenie restauracji

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require_once "includes/db_conn.php";
+?>
 <!DOCTYPE html>
 <html lang="pl">
 
@@ -7,14 +10,6 @@
     <title>Mapa kulinarna</title>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" crossorigin="" />
     <link rel="stylesheet" href="style/main.css">
-    <style>
-        #map {
-            height: 600px;
-            width: 70%;
-            margin-left: auto;
-            margin-right: auto;
-        }
-    </style>
 </head>
 
 <body>
@@ -37,6 +32,26 @@
             attribution: '&copy; OpenStreetMap contributors'
         }).addTo(map);
     </script>
+    <?php
+    $sql = "SELECT nazwa_restauracji,panstwo,miasto,ulica FROM restauracje;";
+    $result = $conn->query($sql);
+    echo '<div class="restauracje-container">';
+    if ($result->num_rows > 0) {
+    
+        while($row = $result->fetch_assoc()) {
+            echo '<div class="restuaracja-box">';
+            echo "<p class='restauracja-nazwa'>" . $row["nazwa_restauracji"]."</p>";
+            echo '<span  class="restauracja-dane">';
+            echo "Państwo: " . $row["panstwo"]."<br>";
+            echo "Adres: " . $row["ulica"].', '.$row["miasto"]."";
+            echo '</span>';
+            echo '</div>';
+        }
+    } else {
+        echo "Brak wyników.";
+    }
+    echo '</div>';
+    ?>
 </body>
 
 </html>
