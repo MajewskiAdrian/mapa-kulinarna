a0ffcd1e5cedcf36dc0b64223b0ce89af70a635b
SzymonGP <grothaszymon@gmail.com>
2024-11-14 11:22:02 +0100

    poprawka błędów

--- index.php
+++ index.php
@@ -18,16 +18,15 @@ require_once "includes/db_conn.php";
     <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" crossorigin=""></script>
     <script>
         var map = L.map('map', {
-            center: [20, 0], // Ustawienie początkowej pozycji mapy
-            zoom: 2, // Początkowy poziom zoomu
-            minZoom: 1.85, // Minimalny poziom zoomu, zapobiega oddaleniu mapy zbyt mocno
-            maxZoom: 6, // Maksymalny poziom zoomu, aby nie dało się przybliżyć za bardzo
+            center: [20, 0], // początkowej pozycja mapy
+            zoom: 2, // początkowy zoom
+            minZoom: 1.85, // min zoom
+            maxZoom: 6, // max zoom
             maxBounds: [
-                [-90, -180], // Lewy dolny róg mapy (maksymalnie na południe i na zachód)
-                [90, 180] // Prawy górny róg mapy (maksymalnie na północ i na wschód)
-            ] // Ogranicza obszar mapy, aby nie można było jej przesunąć poza określony obszar
-        }).setView([20, 0], 2); // Początkowy widok mapy
-
+                [-90, -180], // border (maksymalnie na południe i na zachód)
+                [90, 180] // border (maksymalnie na północ i na wschód)
+            ] // ogólnie sprawia żeby nie można było wyjechać poza określony obszar
+        })
         L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
             attribution: '&copy; OpenStreetMap contributors'
         }).addTo(map);
